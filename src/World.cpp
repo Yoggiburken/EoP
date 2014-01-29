abad52ce535df401ea91395fb15d764ca1fea1af
eldhom <wass95@hotmail.se>
2014-01-29 19:54:28 +0100

    fixed world.load(), no longer deletes the old actor data until the new data is loaded

--- src/World.cpp
+++ src/World.cpp
@@ -20,19 +20,20 @@ sf::Vector2i World::getMousePosition() const
 
 bool World::load(World::LoadType type, sf::Window* window)
 {
-	delete [] actors_;
+	Actor*	temp_actors;
 	actor_count_ = 0; 
 	if(type == World::Menu) {
-		actors_ = new Actor[1];
-		actors_[0].setInputComponent(new ButtonInput());
-		actors_[0].setGraphicsComponent(new ButtonGraphics());
-		actors_[0].position = sf::Vector2f(400, 300);
-		actors_[0].size		= sf::Vector2f(200, 60);
+		temp_actors = new Actor[1];
+		temp_actors[0].setInputComponent(new ButtonInput());
+		temp_actors[0].setGraphicsComponent(new ButtonGraphics());
+		temp_actors[0].position = sf::Vector2f(400, 300);
+		temp_actors[0].size		= sf::Vector2f(200, 60);
 		actor_count_=1;
-		return true;
+		delete [] actors_;
+		actors_ = temp_actors;
 	} else if(type == World::Game) {
 	}
-	return false;
+	return true;
 }
 
 void World::update()
